658a9241853777a7f151ff45906fd53f60356055
zlyysfwin <lianyu.zhang@yasofon.com>
2012-12-24 21:41:48 +0800

    renren android sdk codes, with a bug fixed with userGetInfo--hometown, and added some getter

---
+++ android/trunk/src/com/lingzhimobile/huodonghaowai/cons/RenRenLibConst.java
@@ -0,0 +1,10 @@
+package com.lingzhimobile.huodonghaowai.cons;
+
+public class RenRenLibConst {
+	public static String fieldcommon_access_token = "access_Token";
+	public static String fieldcommon_session_key = "session_Key";
+	public static String fieldcommon_expiration_date = "expiration_Date";
+	public static String fieldcommon_secret_key = "secret_Key";
+	public static String fieldcommon_session_userId = "session_UserId";
+
+}
